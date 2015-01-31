75f7b8bd60c88ade115bdc642c95c6b1cf99553e
Rischan Mafrur <rischanlab@gmail.com>
2015-01-31 19:20:38 +0900

    sample fix for preprocessing3

--- preprocessing3.R
+++ preprocessing3.R
@@ -109,11 +109,57 @@ df_tmp <- rbind(df_activity,df_battery,df_bluetooth,df_call,df_location,df_runap
 
 df <- df_tmp[order(df_tmp$time),]
 
-
-## round time to nearest hour
-df$time <- round(as.POSIXct(df$time), units="hours")
 ## remove weekdays
 x <- df[!weekdays(as.Date(df$time)) %in% c("Saturday", "Sunday"),]
+
+## round time to nearest hour
+df$HP <- round(as.POSIXct(df$time), units="hours")
+library(lubridate)
+g <- transform(df$HP,time.str = format(df$HP,'%H:%M'))
+df$HP <- g$time.str
+df$Weekday <- weekdays(as.Date(df$time))
+
+
+
+
+
+
+
+
+
+View(df)
+
+
+
+
+
+
+
+# round.POSIXct <- function(x, units = c("mins", "5 mins", "10 mins", "15 mins", "quarter hours", "30 mins", "half hours", "hours")){
+#   if(is.numeric(units)) units <- as.character(units)
+#   units <- match.arg(units)
+#   r <- switch(units,
+#               "mins" = 60,
+#               "5 mins" = 60*5,
+#               "10 mins" = 60*10,
+#               "15 mins"=, "quarter hours" = 60*15,
+#               "30 mins"=, "half hours" = 60*30,
+#               "hours" = 60*60
+#   )
+#   H <- as.integer(format(x, "%H")
+#                   M <- as.integer(format(x, "%M"))
+#                   S <- as.integer(format(x, "%S"))
+#                   D <- format(x, "%Y-%m-%d")
+#                   secs <- 3600*H + 60*M + S
+#                   as.POSIXct(round(secs/r)*r, origin=D)
+# }
+# 
+# (x <- Sys.time() + 1:10*60)
+# round(x, "5 mins")
+# round(x, 5)
+# round(x, "10 mins")
+# round(x, "quarter")
+# round(x, "15 mins")
 
 # 
 # 
