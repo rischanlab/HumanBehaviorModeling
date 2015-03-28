a7dc64f91041da89b4b3e844b44cf2b41f81938b
Rischan Mafrur <rischanlab@gmail.com>
2015-03-28 23:56:21 +0900

    without wifi code fixed

--- removing_some_features.R
+++ removing_some_features.R
@@ -1,9 +1,15 @@
 
 ### Removing Location Data
-setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\model\\CSVs")
+# setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\model\\CSVs")
+# 
+# file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\test\\CSVs", full.names = TRUE)
+# file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\model\\CSVs", full.names = TRUE)
+# 
 
-file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\test\\CSVs", full.names = TRUE)
-file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\model\\CSVs", full.names = TRUE)
+#Without Wifi
+file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_wifi\\test\\CSVs_old", full.names = TRUE)
+file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_wifi\\model\\CSVs_old", full.names = TRUE)
+
 
 model_list <- file_list_model
 test_list <- file_list_test
@@ -39,6 +45,29 @@ for (test in test_list){
 
 
 
-#Without Location and Wifi
+#Without Wifi
 
-#etc... 
+
+f_without_wifi <- function(path){
+  data <- read.csv(path, header =TRUE)
+  row_sub <- apply(data,1,function(row) all(row !="wifi"))
+  new_data <- data[row_sub,]
+  return (new_data)
+}
+
+for (model in model_list){
+  data <- f_without_wifi(model)
+  cat(sprintf("Storing data %s to csv file.......",model))
+  cat("\n")
+  new_path <- gsub('CSVs_old', 'CSVs', model)
+  write.csv(data, sprintf("%s",new_path),row.names=FALSE)
+}
+
+for (test in test_list){
+  data <- f_without_wifi(test)
+  cat(sprintf("Storing data %s to csv file.......",test))
+  cat("\n")
+  new_path <- gsub('CSVs_old', 'CSVs', test)
+  write.csv(data, sprintf("%s",new_path),row.names=FALSE)
+}
+
