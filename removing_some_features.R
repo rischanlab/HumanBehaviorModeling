1309715b59d191fccf951a59fe51e3d9ae20ec79
Rischan Mafrur <rischanlab@gmail.com>
2015-03-29 00:40:44 +0900

    added without call and sms

--- removing_some_features.R
+++ removing_some_features.R
@@ -1,24 +1,24 @@
 
-### Removing Location Data
-# setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\model\\CSVs")
-# 
-# file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\test\\CSVs", full.names = TRUE)
-# file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\model\\CSVs", full.names = TRUE)
-# 
-
-#Without Wifi
-file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_wifi\\test\\CSVs_old", full.names = TRUE)
-file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_wifi\\model\\CSVs_old", full.names = TRUE)
+### Removing Some Features Data
 
 
-model_list <- file_list_model
-test_list <- file_list_test
 
 # data_model <- read.csv("ENFP_0719_HP.csv", header=TRUE)
 # head(data_model)
 # 
 # row_sub <- apply(data_model,1,function(row) all(row !="location"))
 # data_model <- data_model[row_sub,]
+
+
+# Without Location
+setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\model\\CSVs")
+
+file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\test\\CSVs", full.names = TRUE)
+file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_location\\model\\CSVs", full.names = TRUE)
+
+
+model_list <- file_list_model
+test_list <- file_list_test
 
 f_without_location <- function(path){
   data <- read.csv(path, header =TRUE)
@@ -46,6 +46,13 @@ for (test in test_list){
 
 
 #Without Wifi
+setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_wifi\\model\\CSVs")
+file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_wifi\\test\\CSVs_old", full.names = TRUE)
+file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_wifi\\model\\CSVs_old", full.names = TRUE)
+
+
+model_list <- file_list_model
+test_list <- file_list_test
 
 
 f_without_wifi <- function(path){
@@ -70,39 +77,16 @@ for (test in test_list){
   new_path <- gsub('CSVs_old', 'CSVs', test)
   write.csv(data, sprintf("%s",new_path),row.names=FALSE)
 }
-
-
-
-#Without Wifi
-
-
-f_without_wifi <- function(path){
-  data <- read.csv(path, header =TRUE)
-  row_sub <- apply(data,1,function(row) all(row !="wifi"))
-  new_data <- data[row_sub,]
-  return (new_data)
-}
-
-for (model in model_list){
-  data <- f_without_wifi(model)
-  cat(sprintf("Storing data %s to csv file.......",model))
-  cat("\n")
-  new_path <- gsub('CSVs_old', 'CSVs', model)
-  write.csv(data, sprintf("%s",new_path),row.names=FALSE)
-}
-
-for (test in test_list){
-  data <- f_without_wifi(test)
-  cat(sprintf("Storing data %s to csv file.......",test))
-  cat("\n")
-  new_path <- gsub('CSVs_old', 'CSVs', test)
-  write.csv(data, sprintf("%s",new_path),row.names=FALSE)
-}
-
 
 
 #Without Activity
+setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_activity\\model\\CSVs")
+file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_activity\\test\\CSVs_old", full.names = TRUE)
+file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_activity\\model\\CSVs_old", full.names = TRUE)
 
+
+model_list <- file_list_model
+test_list <- file_list_test
 
 f_without_activity <- function(path){
   data <- read.csv(path, header =TRUE)
@@ -130,7 +114,13 @@ for (test in test_list){
 
 
 #Without Battery
+setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_battery\\model\\CSVs")
+file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_battery\\test\\CSVs_old", full.names = TRUE)
+file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_battery\\model\\CSVs_old", full.names = TRUE)
 
+
+model_list <- file_list_model
+test_list <- file_list_test
 
 f_without_battery <- function(path){
   data <- read.csv(path, header =TRUE)
@@ -157,7 +147,13 @@ for (test in test_list){
 
 
 #Without Run Apps
+setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_apps\\model\\CSVs")
+file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_runapps\\test\\CSVs_old", full.names = TRUE)
+file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_runapps\\model\\CSVs_old", full.names = TRUE)
 
+
+model_list <- file_list_model
+test_list <- file_list_test
 
 f_without_runapps <- function(path){
   data <- read.csv(path, header =TRUE)
@@ -184,8 +180,43 @@ for (test in test_list){
 
 
 
-#Without Call
+#Without SMS
+setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_sms\\model\\CSVs")
+file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_sms\\test\\CSVs_old", full.names = TRUE)
+file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_sms\\model\\CSVs_old", full.names = TRUE)
+model_list <- file_list_model
+test_list <- file_list_test
 
+f_without_sms <- function(path){
+  data <- read.csv(path, header =TRUE)
+  row_sub <- apply(data,1,function(row) all(row !="sms"))
+  new_data <- data[row_sub,]
+  return (new_data)
+}
+
+for (model in model_list){
+  data <- f_without_sms(model)
+  cat(sprintf("Storing data %s to csv file.......",model))
+  cat("\n")
+  new_path <- gsub('CSVs_old', 'CSVs', model)
+  write.csv(data, sprintf("%s",new_path),row.names=FALSE)
+}
+
+for (test in test_list){
+  data <- f_without_sms(test)
+  cat(sprintf("Storing data %s to csv file.......",test))
+  cat("\n")
+  new_path <- gsub('CSVs_old', 'CSVs', test)
+  write.csv(data, sprintf("%s",new_path),row.names=FALSE)
+}
+
+
+#Without CALL
+setwd("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_call\\model\\CSVs")
+file_list_test <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_call\\test\\CSVs_old", full.names = TRUE)
+file_list_model <- list.files("D:\\Dropbox\\thesis\\PROJECT\\data\\research-result\\without_call\\model\\CSVs_old", full.names = TRUE)
+model_list <- file_list_model
+test_list <- file_list_test
 
 f_without_call <- function(path){
   data <- read.csv(path, header =TRUE)
@@ -209,5 +240,4 @@ for (test in test_list){
   new_path <- gsub('CSVs_old', 'CSVs', test)
   write.csv(data, sprintf("%s",new_path),row.names=FALSE)
 }
-
 
